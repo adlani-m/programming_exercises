3c60cfd9c1aa58be558c4d39a9dd68fe62b704b3
dlani-m <adlanimanar5@gmaile.com>
2025-09-11 19:07:48 +0100

    initial commit

---
+++ exercise3.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+  // Variable declaration
+   int a,b,c,larg;
+
+   printf("Enter Three Numbers :\n");
+   scanf("%d %d %d",&a,&b,&c);
+
+  // Larg among a, b and c
+   larg = a>b ? a>c ? a:c : b>c ? b:c ;
+    printf("Largest Number Among 3 Number : %d",larg);
+}
