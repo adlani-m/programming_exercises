f1d6fc541be4129f8fc3d9345d151a58792340ed
dlani-m <adlanimanar5@gmaile.com>
2025-09-11 19:24:27 +0100

    chnage num 2

--- exercise3.c
+++ exercise3.c
@@ -6,7 +6,7 @@ int main(){
    printf("Enter Three Numbers :\n");
    scanf("%d %d %d",&a,&b,&c);
 
-  // Larg among a, b and and c
+  // Larg among a, b and c
    larg = a>b ? a>c ? a:c : b>c ? b:c ;
     printf("Largest Number Among 3 Number : %d",larg);
 }
